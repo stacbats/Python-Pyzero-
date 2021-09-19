61530ca0dd28545dc0ceaf7a09dd8e48f1945745
Stacy Bates (stacbats) <44994840+stacbats@users.noreply.github.com>
2021-09-19 11:08:51 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+# hello world..again
+
+import pgzrun   # imports zero asset
+def draw():     # function for drawing on screen
+    screen.draw.text("Hello World",topleft=(10, 10))
+    # the above creats the text, easy to follow,
+    # givving the text 1st in "" and then instructions to place it
+pgzrun.go()
