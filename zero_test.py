e485001b8fd41833288a9130b5778a4198cdac3a
Stacy Bates (stacbats) <44994840+stacbats@users.noreply.github.com>
2022-08-06 13:55:18 +0100

    Add files via upload

--- zero_test.py
+++ zero_test.py
@@ -1,19 +1,49 @@
 #py game zero test
 
+
 import pgzrun       # import files to run the pyzero package
 
 
 def update():
-    pass
+    global square_x, square_y, square_x_vel, square_y_vel
+    square_x += square_x_vel 
+ #   square_y +=1
+    if square_x > WIDTH-square_width:
+        square_x= WIDTH-square_width
+        square_x_vel = -square_x_vel 
+    elif square_x < 0:
+        square_x=0
+        square_x_vel= -square_x_vel
+
+    square_y += square_y_vel
+    if square_y > HEIGHT-square_width:
+        square_y= HEIGHT-square_width
+        square_y_vel = -square_y_vel 
+    elif square_y < 0:
+        square_y=0
+        square_y_vel= -square_y_vel
+
 
 
 def draw():                                             # create a function call draw
+    
+    # Clear screen
+    screen.clear()
     #draw a square
-    rectangle = Rect((20,20),(15,15))                 ## first tuple is position of object, second is size
-    screen.draw.filled_rect(rectangle, (255,0,0))
+    rectangle = Rect((square_x,square_y),(square_width, square_height))                 ## first tuple is position of object, second is size
+    screen.draw.filled_rect(rectangle, (255,0,120))
+
 
 WIDTH = 500         # this is setting the window size
 HEIGHT = 300
+
+square_x = 0
+square_x_vel = 3
+square_y = 0
+square_y_vel = 2
+square_width = 15
+square_height = 15
+
 
 pgzrun.go()
 
